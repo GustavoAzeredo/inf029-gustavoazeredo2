78c779a8fccc674bb3f6bb37d81d36acdf876853
Gustavo Azeredo <gustavoazeredo@gmail.com>
2021-03-24 12:57:54 -0300

    lista de funções

--- Atividades/AT04/quest8.c
+++ Atividades/AT04/quest8.c
@@ -4,31 +4,28 @@
 
 typedef struct 
 {
-	char nome[10];
-	char sobrenome[10];
+	char nome[20];
 	int dia;
 	int mes;
 	int ano;
-	double cpf;
-	char sexo[1];
+	char cpf[15];
+	char sexo;
 }novo;
 novo dados;
 
 /*Função 'cadastrarcliente' que lê os dados e 
 os coloca na struct.*/
 
-novo cadastrarcliente(char n[], char sn[], int d, int m, int a, double c, char s[])
+novo cadastrarcliente(char n[], int d, int m, int a, char c[], char s)
 {
 	novo cc;
 
 	printf("Nome: \n");
-	scanf("%s",&cc.nome);
-	printf("sobrenome: \n");
-	scanf("%s",&cc.sobrenome);
+	gets(cc.nome);
 	printf("Digite dia, mês e ano do nascimento: \n");
 	scanf("%d%d%d",&cc.dia,&cc.mes,&cc.ano);
 	printf("Digite o CPF: \n");
-	scanf("%lf",&cc.cpf);
+	scanf("%s",&cc.cpf);
 	printf("Sexo: \n");
 	scanf("%s",&cc.sexo);
 
@@ -39,18 +36,17 @@ novo cadastrarcliente(char n[], char sn[], int d, int m, int a, double c, char s
 
 int main()
 {
-	char name[10];
-	char lastname[10];
-	char sex[5];
+	char name[20];
+	char sex;
 	int day, month, year;
-	double ssn;
+	char ssn[15];
 
-	novo dados = cadastrarcliente(name, lastname, day, month, year, ssn, sex);
+	novo dados = cadastrarcliente(name, day, month, year, ssn, sex);
 
-	printf("- Nome: %s %s\n",dados.nome,dados.sobrenome);
+	printf("- Nome: %s\n",dados.nome);
 	printf("- Data de nascimentto: %d/%d/%d\n",dados.dia,dados.mes,dados.ano);
-	printf("- CPF: %.0lf\n",dados.cpf);
-	printf("- Sexo: %s\n",dados.sexo);
+	printf("- CPF: %s\n",dados.cpf);
+	printf("- Sexo: %c\n",dados.sexo);
 
 	return 0;
 }
